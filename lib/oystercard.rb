8f7b86345c0833a63176deefe6ea9965498ddc39
adam.beresford <adam56.beresford@gmail.com>
2019-10-08 14:38:50 +0100

    Almost completed 12

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,39 @@
+
 class Oystercard
-  attr_accessor :balance
+  attr_accessor :balance, :in_journey, :stations
   CARD_MAX_LIMIT = 90
+  CARD_MIN_LIMIT = 1
+  MIN_FARE = 2
 
   def initialize
     @balance = 0
+    @in_journey = nil
+    @stations = []
   end
 
   def top_up(money)
-    fail "Card limit is #{CARD_LIMIT}" if @balance + money > CARD_LIMIT
+    fail "Card limit is £#{CARD_MAX_LIMIT}" if @balance + money > CARD_MAX_LIMIT
     @balance += money
   end
 
+  def touch_in(station)
+    @starting_station = station
+  end
+
+  def touch_out(money = MIN_FARE, ending_station = "Bakerloo")
+    deduct(money)
+    @stations << {:starting_station => @starting_station, :ending_station => ending_station}
+    @starting_station = nil
+  end
+
+  def in_journey?
+    @in_journey = (@starting_station != nil)
+  end
+
+  private
+
   def deduct(money)
+    fail "insufficient funds £#{CARD_MIN_LIMIT}" if @balance - money < CARD_MIN_LIMIT
     @balance -= money
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,10 +7,13 @@ describe Oystercard do
     it 'creates empty account' do
       expect(subject.balance).to eq(0)
     end
-
   end
 
   describe '#top_up' do
+
+    it 'balance cap' do
+      expect{subject.top_up(92)}.to raise_error "Card limit is £#{Oystercard::CARD_MAX_LIMIT}"
+    end
 
     it 'has been topped up' do
       expect(subject).to receive(:top_up).with(20)
@@ -21,25 +24,52 @@ describe Oystercard do
       subject.top_up(20)
       expect(subject.balance).to_not eq(0)
     end
-
   end
 
   describe '#deduct' do
 
-    it 'balance cap' do
-      expect{subject.top_up(92)}.to raise_error "Card limit is #{Oystercard::CARD_MAX_LIMIT}"
-    end
-
     it 'raises an error when balance is below fare' do
-      expect{subject.deduct(20)}.to raise_error 'insufficient balance'
-
+      expect{subject.touch_out(20)}.to raise_error "insufficient funds £#{Oystercard::CARD_MIN_LIMIT}"
     end
 
     it 'deducted from balance' do
-      subject.deduct(20)
-      expect(subject.balance).to eq(-20)
+      subject.top_up(30)
+      subject.touch_out(20)
+      expect(subject.balance).to eq(10)
     end
-
   end
 
+  #journey
+
+  describe '#touch_in' do
+
+    it { is_expected.to respond_to(:touch_in) }
+
+    it 'is active after touch in' do
+      subject.touch_in("Charring Cross")
+      expect(subject.in_journey?).to eq true
+      expect(subject.stations[0]).to include("Charring Cross")
+    end
+  end
+
+  describe '#touch_out' do
+
+    it { is_expected.to respond_to(:touch_out) }
+
+    it 'is inactive after touch out' do
+
+      subject.top_up(20)
+
+
+      expect {subject.touch_out}.to change{subject.balance}.by(-2)
+      expect(subject.stations[0]).to include("Bakerloo")
+      expect(subject.in_journey?).to eq false
+    end
+  end
+
+  describe '#in_journey' do
+
+    it { is_expected.to respond_to(:in_journey) }
+
+  end
 end
